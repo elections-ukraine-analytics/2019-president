995be748590c57531b94b5316a057d6318f95ad5
elections-ukraine <e.control.2019.ak@gmail.com>
2019-04-19 23:52:33 +0300

    Data - e-vybory - extend

--- scripts/src/evybory-protocols-compact.js
+++ scripts/src/evybory-protocols-compact.js
@@ -4,7 +4,7 @@ module.exports = (raw) => {
   const result = {};
 
   for (const row of data) {
-    const { area_code, ps_code, has_errors, photo_slug, table_slug } = row;
+    const { area_code, ps_code, has_errors, photo_slug, table_slug, photo_date, table_date } = row;
     const rZ = +row.r11_14;
     const rP = +row.r11_30;
     const rSum = Object.keys(row).filter(key => key.startsWith('r11_')).reduce((prevValue, curValue) => (prevValue + +row[curValue]), 0);
@@ -15,7 +15,7 @@ module.exports = (raw) => {
     if (!result[key]) {
       result[key] = [];
     }
-    result[key].push([area_code, ps_code, has_errors, photo_slug, table_slug, rZ, rP, rSum, rppZ, rppP]);
+    result[key].push([area_code, ps_code, has_errors, photo_slug, table_slug, photo_date, table_date, rZ, rP, rSum, rppZ, rppP]);
   }
 
   return {
